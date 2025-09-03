ff41dd20a85282b3a3cdabdf4dcd1333de9372de
TEKim <kte9411@gmail.com>
2025-09-03 13:33:04 +0900

    deploy : 백엔드 배포 설정

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("./middleware/passport");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const testsRoutes = require("./routes/tests-routes");
 const commentsRoutes = require("./routes/comments-routes");
@@ -10,17 +11,24 @@ const HttpError = require("./models/http-error");
 
 const app = express();
 
-app.use(express.json());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://pickq.im",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.use("/api/tests", testsRoutes);
 
@@ -41,12 +49,15 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "알 수 없는 오류가 발생했습니다." });
 });
 
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cxqro4z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(PORT);
+    console.log(`Server running on port ${PORT}`);
   })
   .catch((err) => {
     console.log(err);
